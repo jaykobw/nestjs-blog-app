30ae40c0e541dae7ac99875570030e234863094f
jaykobw <watenga.jacob@gmail.com>
2025-01-29 17:20:45 +0300

    chore: code refactor

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,7 +5,7 @@ export default registerAs('database', () => ({
   port: parseInt(process.env.DATABASE_PORT) || 5432,
   user: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_USER || 'postgres',
-  name: process.env.DATABASE_NAME || 'nestjs-blog',
+  name: process.env.DATABASE_TEST_NAME || 'nestjs-blog',
   synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
   autoLoadEntities: process.env.DATABASE_AUTOLOAD === 'true' ? true : false,
   logging: process.env.DATABASE_LOGGING === 'true' ? true : false,
--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { config } from 'aws-sdk';
-import helmet from 'helmet';
-import { ConfigService } from '@nestjs/config';
+import { appCreate } from './app.create';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  /**
-   * Use validation pipes globally
-   */
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
 
   /**
-   * Swagger documentation
+   * Add middleware
    */
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('NestJS Masterclass - Blog App API')
-    .setDescription('Use the base API URL as http://localhost:3000')
-    .setTermsOfService('http://localhost:3000/terms-of-service')
-    .setLicense(
-      'MIT License',
-      'https://github.com/git-scm.com/blob/main/MIT-LICENSE.txt',
-    )
-    .addServer('http://localhost:3000')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
-
-  // AWS SDK
-  const configService = app.get(ConfigService);
-  config.update({
-    credentials: {
-      accessKeyId: configService.get('appConfig.awsAccessKeyId'),
-      secretAccessKey: configService.get('appConfig.awsSecretAccessKey'),
-    },
-    region: configService.get('appConfig.awsRegion'),
-  });
-
-  //enable cors
-  app.enableCors();
-
-  // helmet
-  app.use(helmet());
-
-  // Disable X-Powered-By header
-  app.getHttpAdapter().getInstance().disable('x-powered-by');
+  appCreate(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
